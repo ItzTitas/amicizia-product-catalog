f4bca20e5841243a5928496309e2918cf231a75e
Amicizia <info@amicizialifescience.com>
2026-02-13 13:42:34 +0530

    Update site title to include full product range

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from "next";
 import HomePage from "./home-page";
-
-
-
+export const metadata: Metadata = {
+  title: "Amicizia Life Science | Veterinary,Livestock,Canine & Poultry Healthcare Products in India",
+};
 
 export default function Home() {
   return <HomePage />;
